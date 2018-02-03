9a8082deb5ae72e35e4f485c65ca1a7848101e9a
Mateusz Zylka <mateuszzylkaa@gmail.com>
2018-02-03 20:40:44 +0100

    Change in script files

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
-var a = prompt('Enter value a:'),
-	h = prompt('Enter value h:'),
+//variable: base, height, field calculation
+
+var a = prompt(' value a:'),
+	h = prompt('Provide value h:'),
 	triangleArea = a*h/2;
+
+//calculation of the triangle field result
 
 alert('Triangle field is ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
