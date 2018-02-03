5e6408f06cbc6b069ecfa813e6dae6b829a53fb5
Mateusz Zylka <mateuszzylkaa@gmail.com>
2018-02-03 20:25:30 +0100

    Task 8_5

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter value a:'),
+	h = prompt('Enter value h:'),
+	triangleArea = a*h/2;
+
+alert('Triangle field is ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
